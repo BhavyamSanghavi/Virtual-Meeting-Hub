c8de3f4001b72650486a3a433e26a15f6b4af4fc
Bhavyam Sanghavi <115462395+BhavyamSanghavi@users.noreply.github.com>
2024-06-29 17:54:46 +0530

    Update page.tsx

--- app/(auth)/sign-in/[[...sigin-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sigin-in]]/page.tsx
@@ -2,8 +2,14 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function SiginInPage() {
   return (
-    <main className="flex h-screen w-full items-center justify-center">
-      <SignIn />
-    </main>
+    <main className="flex h-screen w-full items-center justify-center text-white">
+  <div className="flex flex-col items-center">
+    <div className="mb-4 text-center text-2xl">
+      <p>Email: abc@gmail.com</p>
+      <p>Password: abc123</p>
+    </div>
+    <SignIn />
+  </div>
+</main>
   );
 }
